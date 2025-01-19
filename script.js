0a22d489273d1f09f934d04a956aac5a4ac02e8a
captainfaisal13 <shaikhfaisal3412@gmail.com>
2025-01-19 15:13:41 +0530

    feat: comment script file

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
-const Tweet = require("./models/Tweet"); // Adjust the path as needed
+// require("dotenv").config();
+// const mongoose = require("mongoose");
+// const Tweet = require("./models/Tweet"); // Adjust the path as needed
 
-const addNewFields = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
+// const addNewFields = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
 
-    // Update all documents to include the new fields with default values
-    await Tweet.updateMany(
-      {},
-      {
-        $set: {
-          shareCount: 0,
-        },
-      }
-    );
+//     // Update all documents to include the new fields with default values
+//     await Tweet.updateMany(
+//       {},
+//       {
+//         $set: {
+//           shareCount: 0,
+//         },
+//       }
+//     );
 
-    console.log("New fields added to all tweets successfully!");
-    mongoose.connection.close();
-  } catch (error) {
-    console.error("Error updating tweets:", error);
-    mongoose.connection.close();
-  }
-};
+//     console.log("New fields added to all tweets successfully!");
+//     mongoose.connection.close();
+//   } catch (error) {
+//     console.error("Error updating tweets:", error);
+//     mongoose.connection.close();
+//   }
+// };
 
-addNewFields();
+// addNewFields();
